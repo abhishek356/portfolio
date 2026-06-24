1cf2dee7392462142869abfb01284bc789661a21
abhishek356 <abhishek867492@gmail.com>
2026-06-24 07:20:19 +0000

    init commit for gcp deployment

---
+++ src/components/CanvasLoader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import{Html, useProgress} from '@react-three/drei'
+const CanvasLoader = () => {
+  const { progress } = useProgress()
+  return (
+    <Html as= "div" center style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+        <span className = "canvas-loader">
+            <p style = {{color: 'white', fontSize: 14, fontWeight: 800, marginTop: 40}}>{progress !== 0 ? `${progress.toFixed(2)}%` : "Loading..."}</p>
+        </span>
+    </Html>
+  )
+}
+
+export default CanvasLoader
