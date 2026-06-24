3f8e69d21b66eb8a9f8b1f057903ac1eaf461939
abhishek356 <abhishek867492@gmail.com>
2026-06-24 08:15:21 +0000

    fix build errors

--- src/components/CanvasLoader.tsx
+++ src/components/CanvasLoader.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import{Html, useProgress} from '@react-three/drei'
+import { Html, useProgress } from '@react-three/drei'
 const CanvasLoader = () => {
   const { progress } = useProgress()
   return (
